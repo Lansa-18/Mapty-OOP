14304c1c162cb384934659e9449db5dc6e1504ea
Mapelujo <olamideiyanda18@gmail.com>
2023-11-08 21:43:06 +0100

    progress

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Using the Geolocation Api.
+navigator.geolocation.getCurrentPosition(function(), function(){
+    
+}) // Takes in 2 callback functions. One that get's called on success and the second that get's called when there is an error
