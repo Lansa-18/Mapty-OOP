cb99e4f51dd02b31eb764137d1772d0b358faf6a
Mapelujo <olamideiyanda18@gmail.com>
2023-11-09 21:20:04 +0100

    progress

--- script.js
+++ script.js
@@ -24,20 +24,21 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude]
       const map = L.map('map').setView(coords, 13);
-      console.log(map);
+    //   console.log(map);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
+      map.on('click', function(mapEvent){
+        console.log(mapEvent);
+
+        L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS popup.<br> Easily customizable.')
         .openPopup();
 
-      map.on('click', function(mapEvent){
-        console.log(mapEvent);
       })
     },
     function () {
