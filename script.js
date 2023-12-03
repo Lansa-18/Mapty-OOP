23cf597e430dd672e6cb31e65c1d38d277ed48c1
Mapelujo <olamideiyanda18@gmail.com>
2023-12-03 20:47:20 +0100

    progress

--- script.js
+++ script.js
@@ -26,6 +26,16 @@ class Workout {
     this.distance = distance; // in km
     this.duration = duration; // in min
   }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 
+    'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(
+      1
+    )} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+  }
 }
 
 // CHILDREN WORKOUT CLASSES
@@ -198,10 +208,12 @@ class App {
 
   _renderWorkout(workout) {
     const html = `
-    <li class="workout workout--${workout.name}" data-id="${workout.id}">
+    <li class="workout workout--${workout.type}" data-id="${workout.id}">
       <h2 class="workout__title">Running on April 14</h2>
       <div class="workout__details">
-        <span class="workout__icon">${workout.name === 'running' ? '🏃‍♂️' : '🚴‍♀️' }</span>
+        <span class="workout__icon">${
+          workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+        }</span>
         <span class="workout__value">${workout.distance}</span>
         <span class="workout__unit">km</span>
       </div>
