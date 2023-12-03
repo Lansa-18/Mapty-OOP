9e976f12f16bf77af903fee11eace0fec84036d6
Mapelujo <olamideiyanda18@gmail.com>
2023-12-03 20:58:58 +0100

    progress

--- script.js
+++ script.js
@@ -246,12 +246,12 @@ class App {
       html += `
         <div class="workout__details">
           <span class="workout__icon">⚡️</span>
-          <span class="workout__value">16</span>
+          <span class="workout__value">${workout.speed}</span>
           <span class="workout__unit">km/h</span>
         </div>
         <div class="workout__details">
           <span class="workout__icon">⛰</span>
-          <span class="workout__value">223</span>
+          <span class="workout__value">${workout.elevationGain}</span>
           <span class="workout__unit">m</span>
         </div>
       </li>
