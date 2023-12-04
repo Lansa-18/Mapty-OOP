d86567f80f6484d0dd585b20fe115ade0335abe3
Mapelujo <olamideiyanda18@gmail.com>
2023-12-04 06:24:14 +0100

    progress

--- script.js
+++ script.js
@@ -312,7 +312,7 @@ class App {
   }
 
   _getLocalStorage(){
-    const data = localStorage.getItem('workouts')
+    const data = JSON.parse(localStorage.getItem('workouts'));
     console.log(data);
   }
 }
